a8f5919c2aff0c191de4c48b17e602ea47cc694a
Ramazan <rmzncck65@gmail.com>
2023-06-20 15:47:54 +0300

    up

--- src/component/UserCard.js
+++ src/component/UserCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 const UserCard = ({username, id, name, email}) => {
   return (
     <div className='userdiv'>
-        <h3>User{id}</h3>
+        <h3>User{id} </h3>
         <h5>Name: {name}</h5>
         <h5>Username : {username}</h5>
         <h5>Email : {email}</h5>
